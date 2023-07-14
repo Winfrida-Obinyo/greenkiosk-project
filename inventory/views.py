98d7cb9c3136c55ef453ead9939ece4d1d43d348
BurajeVallary <vallaryburaje@gmail.com>
2023-07-14 17:11:28 +0300

    added a form

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import ProductuploadForm
+
 
 # Create your views here.
+
+# function based views
+# request rep a http request   
+# render accept request,the template to render the request and data to render the request
+
+
+def upload_product(request):
+    form =ProductuploadForm()
+    return render(request,"inventory/product_upload.html",{"form":form})
+
+
--- review/admin.py
+++ review/admin.py
@@ -8,3 +8,4 @@ from .models import Review
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('product_name','rating', 'comment', 'date_created')
+
